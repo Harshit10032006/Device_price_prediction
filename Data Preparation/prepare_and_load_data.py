23fd11977c75189de8ddcdba7e403705af05b081
Harshit <harshitkholiya1003@gmail.com>
2026-01-22 09:52:44 +0530

    Create prepare_and_load_data.py

---
+++ Data Preparation/prepare_and_load_data.py
@@ -0,0 +1,54 @@
+import pandas as pd 
+from sqlalchemy import create_engine
+
+
+data=pd.read_csv('phone_price.csv')   
+
+
+data.rename(columns={
+    "Battery capacity": "BatteryCapacity",
+    "Screen size": "ScreenSize",
+    "RAM (MB)": "RAM_MB",
+    "Internal storage (GB)": "InternalStorage_GB",
+    "4G/LTE": "Has4G_LTE",
+    "Wi-Fi": "HasWiFi"
+}, inplace=True)
+data.to_csv('phone_price.csv',index=False)
+
+engine= create_engine( "mssql+pyodbc://@harshit\\SQLEXPRESS/DEVICES"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
+    "&trusted_connection=yes")
+data.to_sql('Phones',engine,index=False)
+
+
+lap=pd.read_csv(r"C:\Users\kholi\datascience\Project Phones\laptop_price.csv")
+lap.rename(columns={'Ram':'RAM_MB','Company':'Brand'},inplace=True)
+lap.rename(columns={'Price_euros':'Price'},inplace=True)
+lap.rename(columns={'Inches':'Screen size (inches)','Product':'Name'},inplace=True)
+
+lap['RAM_MB']=lap['RAM_MB'].str.replace('GB',"")
+lap['RAM_MB'] = pd.to_numeric(lap['RAM_MB'])   
+
+lap['RAM_MB']=lap['RAM_MB']*1024
+lap['Price']=lap['Price']*107.22
+
+
+lap.drop(columns=['Unnamed: 16'], inplace=True)
+
+
+columns=lap['ScreenResolution'].str.findall(r'\d+')
+lap['Resolution x']=columns.str[0].astype('Int64')
+lap['Resolution y']=columns.str[1].astype('Int64')
+
+lap.drop('ScreenResolution',axis=1,inplace=True)
+lap.to_csv('laptop_price.csv',index=False)
+
+lap.to_sql('Laptop',engine,index=False)
+
+
+
+
+
+
+
+
